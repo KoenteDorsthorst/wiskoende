7e77fab9fdfa67100f5cb3f0c7927190b6ca5bec
Koen te Dorsthorst <KoenteDorsthorst@gmail.com>
2021-12-21 22:49:12 +0100

    Push so I can switch IDE
    it does not work right now

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+
+
+<?php include "Includables/navbar.html" ?>
+
+
+
+
+<div class="main">
+
+    <h1> WISKOENDE </h1>
+
+<!-- Vraagstelling (Heb jij hulp nodig?) -->
+<h3> Hulp bij wiskunde!</h3>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+
+
+<h3> Wat kan je hier doen?</h3>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+<h3> </h3>
+
+</div>
+
+</body>
+</html>
