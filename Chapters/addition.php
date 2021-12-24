8d5548a80b860271f58a7edb138d73ce134ed276
Koen te Dorsthorst <KoenteDorsthorst@gmail.com>
2021-12-24 20:56:29 +0100

    Added chapters and more includables

---
+++ Chapters/addition.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../css.css">
+</head>
+<body>
+
+
+<?php include "../Includables/navbar.html" ?>
+
+<div class="main">
+
+    <h3> Uitleg </h3>
+    <p> Optellen is makkelijk. Je telt een getal bij het andere op.</p>
+
+
+    <h3> Oefenen </h3>
+    <a href="main.php"> oefeningen </a>
+
+</div>
+
+
+
+
+</body>
+</html>
--- main.php
+++ main.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 
+
 <?php include "Includables/navbar.html" ?>
 
 <script>
