538b1cd97b88244dcf5aba4d4fca8cafac601056
Koen te Dorsthorst <KoenteDorsthorst@gmail.com>
2021-12-27 18:47:00 +0100

    Moved tests to their own volder, added topbar

---
+++ Chapters/Pythagoras/pythagoras.php
@@ -0,0 +1,31 @@
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/Includables/begin.html";
+include($path) ?>
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/Includables/navbar.html";
+include($path) ?>
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/Includables/topbar.html";
+include($path) ?>
+
+
+<div class="main">
+    <h3>Wat is de pythagoras formule?</h3>
+    <p> a² + b² = c²</p>
+
+
+
+    <h3>Opdrachten</h3>
+    <a href="/Chapters/Pythagoras/pythagorasTest1.php">test</a>
+</div>
+
+
+
+
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/Includables/end.html";
+include($path) ?>
